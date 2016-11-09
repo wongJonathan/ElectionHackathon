870ab23fd0cc505a26c54594d2bf5a82049f5e89
Jonathan Wong <wong.jonathan@live.com>
2016-11-08 19:38:13 -0500

    Added migrations

--- TrumpBt/TheButton/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class ThebuttonConfig(AppConfig):
-    name = 'TrumpBt.TheButton'
--- TrumpBt/settings.py
+++ TrumpBt/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'theButton.apps.ThebuttonConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
