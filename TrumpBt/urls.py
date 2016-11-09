896a64fcdf74377757383ba103be7a0e7e8d9a9a
angbamfield <angelabamfield@gmail.com>
2016-11-08 20:17:22 -0500

    Add files via upload
    urls.py modified for the quoptes

--- TrumpBt/urls.py
+++ TrumpBt/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.conf.urls import  url
+from django.conf.urls import include
+import theButton.urls
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^quote/', include(theButton.urls)),
 ]
